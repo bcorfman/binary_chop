e7afc7d258cd95869f27dd4421152776798c0572
Brandon Corfman <bcorfman@fastmail.fm>
2022-08-13 13:11:23 -0400

    Day 3 completed

---
+++ src/day3.py
@@ -0,0 +1,16 @@
+def _chop(tgt, lst, start, end):
+    result = -1
+    mid = start + (end - start) // 2
+    if tgt == lst[start]:
+        print(result)
+        result = start 
+    elif mid - start == 0:
+        result = -1
+    elif tgt < lst[mid]:
+        result = _chop(tgt, lst, start, mid)
+    else:
+        result = _chop(tgt, lst, mid, end)
+    return result
+
+def chop(tgt, lst):
+    return _chop(tgt, lst, 0, len(lst)) if lst else -1
